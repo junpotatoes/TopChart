59f207db469f7f73eb304e1e3758116ccac42cf9
junpotatoes <mukdol1127@naver.com>
2023-04-15 01:14:38 +0900

    #3 - feat: TopChart 컴포넌트 router 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import { ThemeProvider } from 'styled-components';
 import theme from './styles/theme.style';
 import { GlobalStyle } from './styles/global.style';
+import { Route, Routes } from 'react-router-dom';
+import TopChart from './components/TopChart';
 
 const App = () => {
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyle />
-
-      <div>hello world</div>
+      <Routes>
+        <Route path="/" element={<TopChart />} />
+      </Routes>
     </ThemeProvider>
   );
 };
