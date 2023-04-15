92dd87b485c86d3c02f3e28c7cb9fafeff225a40
최준호 <103482590+junpotatoes@users.noreply.github.com>
2023-04-15 12:29:35 +0900

    프로젝트 절대경로 수정 (#16)
    * #15 - chore: 절대경로 환경 설정
    
    * #15 - chore: 절대경로 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 import { ThemeProvider } from 'styled-components';
-import theme from './styles/theme.style';
-import { GlobalStyle } from './styles/global.style';
 import { Route, Routes } from 'react-router-dom';
-import TopChart from './components/TopChart';
-import TrackDetail from './components/TrackDetail';
+import { GlobalStyle } from '@/styles/global.style';
+import theme from '@/styles/theme.style';
+import TopChart from '@/components/TopChart';
+import TrackDetail from '@/components/TrackDetail';
 
 const App = () => {
   return (
